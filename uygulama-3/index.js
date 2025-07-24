7827038bcba2d8ac877bbad70d593eff40406809
yakup berhuni <yakupberhuni@gmail.com>
2025-07-24 13:53:21 +0300

    main

---
+++ uygulama-3/index.js
@@ -0,0 +1,11 @@
+let myName = "yakup";
+let longstring = "Merahbaler ben Yakup berhuni";
+
+console.log(myName);
+console.log(myName.length);
+console.log(longstring);
+console.log(longstring.replaceAll(" ", ""));
+console.log(longstring.split(" "));
+console.log(longstring.toUpperCase());
+console.log(longstring.toLowerCase());
+console.log(longstring.indexOf("yakup"));
